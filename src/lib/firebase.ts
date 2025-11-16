f8394f5c972bff505c13ddb56c9601662498c262
Ludovic Aggaï <sepogonangabang@gmail.com>
2025-11-16 17:03:52 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,16 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: "AIzaSyAl6HpFgh1qa3toIaExy9ctnN-mxcq3Gc4",
-    authDomain: "onbush-df5af.firebaseapp.com",
-    projectId: "onbush-df5af",
-    storageBucket: "onbush-df5af.firebasestorage.app",
-    messagingSenderId: "824666553967",
-    appId: "1:824666553967:web:f620ee04187a6baad8979a"
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDhtgENbLR-VhC-1SFBUTL6bEes5sdv8Jc",
+    authDomain: "luvvix-78a47.firebaseapp.com",
+    projectId: "luvvix-78a47",
+    storageBucket: "luvvix-78a47.firebasestorage.app",
+    messagingSenderId: "316210140325",
+    appId: "1:316210140325:web:cb1da5a4e84cff172a694c",
+    measurementId: "G-X1HTGJKNZ5"
+  
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
